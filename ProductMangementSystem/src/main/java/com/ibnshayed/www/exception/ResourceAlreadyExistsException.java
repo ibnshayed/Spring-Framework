ecb506a5821ec2ef8e8e8756c949541caa3763ed
ibnshayed <emranibnshayed@gmail.com>
2020-01-06 18:28:08 +0600

    ibnshayed

--- ProductMangementSystem/src/main/java/com/ibnshayed/www/exception/ResourceAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.ibnshayed.www.exception;
-
-public class ResourceAlreadyExistsException extends Exception {
-    public ResourceAlreadyExistsException(String id) {
-        super(id + " already exists!");
-    }
-
-}
